ee4ebed92edc67d8e885995e63cb8b913bd26c18
yaider <yaideralejo2@gmail.com>
2026-02-24 12:45:52 -0500

    primer commit funcional

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 use std::path::PathBuf;
-use yt_dlp::Youtube;
+use yt_dlp::Downloader;
 use yt_dlp::client::deps::Libraries;
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let libraries_dir = PathBuf::from("libs");
-    let output_dir = PathBuf::from("output");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use std::env;
 
-    let youtube = libraries_dir.join("yt-dlp");
-    let ffmpeg = libraries_dir.join("ffmpeg");
+    unsafe {
+        env::set_var("YT_DLP_NO_WARNINGS", "1");
+        env::set_var("YT_DLP_UPDATE", "0");
+    }
 
-    let libraries = Libraries::new(youtube, ffmpeg);
-    let fetcher = Youtube::new(libraries, output_dir).await?;
+    let libraries = Libraries::new(
+        PathBuf::from("libs/yt-dlp.exe"),
+        PathBuf::from("libs/ffmpeg.exe"),
+    );
+    let downloader = Downloader::builder(libraries, "output").build().await?;
 
-    let url = String::from("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    let video_path = fetcher.download_video_from_url(url, "my-video.mp4").await?;
+    // Aqui hago la magia
+    let url = String::from(
+        "https://www.youtube.com/watch?v=WXIfT-dQtCo&pp=ygUXcHJvbWVzYXMgc29icmUgZWwgYmlkZXQ%3D",
+    );
+    let video = downloader.fetch_video_infos(url).await?;
+    let _video_path = downloader
+        .download_video(&video, "Promesas_sobre_el_bidet.mp4")
+        .await?;
+    println!("OK");
     Ok(())
 }
